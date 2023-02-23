a8b844d8c22912b47221af38e1212b630f0a51ec
usamaiftikhar530 <usamaiftikhar530@gmail.com>
2023-02-23 11:42:27 +0500

    fifth commit

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -9,7 +9,7 @@ function Logout(){
     const navigate = useNavigate();
 
     useEffect(()=>{
-        fetch(url+"/logout",{
+        fetch("/logout",{
             method: "GET",
             headers: {
                 Accept: "application/json",
